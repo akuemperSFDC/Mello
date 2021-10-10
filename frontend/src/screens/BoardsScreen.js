e622374c963d11eb0fda493230f5faa65d8e5599
Austin Kuemper <78562233+AKuemper@users.noreply.github.com>
2021-10-09 19:30:13 -0700

    added toast error message when creating a board

--- frontend/src/screens/BoardsScreen.js
+++ frontend/src/screens/BoardsScreen.js
@@ -1,15 +1,17 @@
+import { useState, useEffect } from 'react';
 import { Grid, Paper, Typography } from '@mui/material';
 import * as M from '@mui/material';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getBoardsAsync } from '../features/boards/boardSlice.js';
 import SideBar from '../components/ui/SideBar.js';
+import Toast from '../components/utils/Toast.js';
 import { createBoardModal } from '../features/modal/modalSlice.js';
 
 const BoardsScreen = () => {
   const dispatch = useDispatch();
-  const { boards } = useSelector((s) => s.boards) || [];
+  const [open, setOpen] = useState(false);
+  const { boards, errors, loading } = useSelector((s) => s.boards) || [];
 
   const theme = M.useTheme();
 
@@ -17,92 +19,112 @@ const BoardsScreen = () => {
     dispatch(getBoardsAsync());
   }, [dispatch]);
 
-  return (
-    <Grid
-      container
-      flexGrow={1}
-      sx={{
-        mt: theme.mixins.denseToolbar.minHeight,
-        p: 1,
-        maxWidth: 'lg',
-        mx: 'auto',
-      }}
-    >
-      {/* Sidebar */}
-      <SideBar />
+  useEffect(() => {
+    if (errors) {
+      setOpen(true);
+    }
+  }, [errors]);
 
-      {/* Main content */}
-      <Grid item md={9}>
-        <Grid container sx={{ p: 2, mt: 3 }}>
-          <Grid item>
-            <Typography variant='h6'>Your boards</Typography>
+  return (
+    <>
+      {errors &&
+        errors.map((error) => (
+          <Toast open={open} setOpen={setOpen}>
+            {error}
+          </Toast>
+        ))}
+      <Grid
+        container
+        flexGrow={1}
+        sx={{
+          mt: theme.mixins.denseToolbar.minHeight,
+          p: 1,
+          maxWidth: 'lg',
+          mx: 'auto',
+        }}
+      >
+        {/* Sidebar */}
+        <SideBar />
+
+        {/* Main content */}
+        <Grid item md={9}>
+          <Grid container sx={{ p: 2, mt: 3 }}>
+            <Grid item>
+              <Typography variant='h6'>Your boards</Typography>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container sx={{ px: 2 }}>
-          {boards &&
-            boards.map((board) => (
-              <Grid
-                key={board._id}
-                item
-                component={Link}
-                to={`/b/${board._id}`}
-                sx={{
-                  textDecoration: 'none',
-                  mb: 3,
-                  mr: 2,
-                  '&:hover': {
-                    opacity: 0.8,
-                  },
-                }}
-              >
-                <Paper
+          <Grid container sx={{ px: 2 }}>
+            {boards &&
+              boards.map((board) => (
+                <Grid
+                  key={board._id}
+                  item
+                  component={Link}
+                  to={`/b/${board._id}`}
                   sx={{
-                    minWidth: '11.5rem',
-                    backgroundImage: `url(${board.backgroundImage})`,
-                    height: '6rem',
-                    backgroundPosition: 'center',
+                    textDecoration: 'none',
+                    mb: 3,
+                    mr: 2,
+                    '&:hover': {
+                      opacity: 0.8,
+                    },
                   }}
                 >
                   <Paper
                     sx={{
                       minWidth: '11.5rem',
-                      backgroundColor: 'rgba(0, 0, 0, 0.13)',
+                      maxWidth: '11.5rem',
+                      backgroundImage: `url(${board.backgroundImage})`,
                       height: '6rem',
+                      backgroundPosition: 'center',
                     }}
                   >
-                    <Typography
-                      gutterBottom
-                      component='div'
-                      sx={{ color: 'white', p: 1 }}
+                    <Paper
+                      sx={{
+                        minWidth: '11.5rem',
+                        backgroundColor: 'rgba(0, 0, 0, 0.13)',
+                        height: '6rem',
+                      }}
                     >
-                      {board.title}
-                    </Typography>
+                      <Typography
+                        gutterBottom
+                        component='div'
+                        sx={{
+                          color: 'white',
+                          p: 1,
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                        }}
+                      >
+                        {board.title}
+                      </Typography>
+                    </Paper>
                   </Paper>
-                </Paper>
-              </Grid>
-            ))}
+                </Grid>
+              ))}
 
-          {/* Add new board button */}
-          <Grid item>
-            <Paper
-              onClick={() => dispatch(createBoardModal(true))}
-              component={M.Button}
-              disableRipple
-              sx={{
-                minWidth: '11.5rem',
-                backgroundColor: 'common.grey',
-                height: '6rem',
-                mr: 2,
-              }}
-            >
-              <Typography gutterBottom component='div'>
-                Create a new board
-              </Typography>
-            </Paper>
+            {/* Add new board button */}
+            <Grid item>
+              <Paper
+                onClick={() => dispatch(createBoardModal(true))}
+                component={M.Button}
+                disableRipple
+                sx={{
+                  minWidth: '11.5rem',
+                  backgroundColor: 'common.grey',
+                  height: '6rem',
+                  mr: 2,
+                }}
+              >
+                <Typography gutterBottom component='div'>
+                  Create a new board
+                </Typography>
+              </Paper>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
